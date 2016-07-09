9a981d6c0baa0331424ada514c1e4632a7f48d04
markliebs <markliebs@me.com>
2016-07-08 23:45:13 -0400

    Fixed naming errors and added files for heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured, date) VALUES ('Cheese Burger', true, now());
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Double Bacon Cheese Burger', true, now());
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Cali Burger with Guaq', true, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon Double Cheese Burger', true, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bison Burger with Bacon', true, now());
